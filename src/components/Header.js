8bef5211fcfa3d47cc08e65d90205c47de06f2b5
-THE-LAST-SAMURAI- <connor813@outlook.com>
2022-05-17 15:51:39 -0500

    added basic select quiz menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,20 @@
 import React from "react";
+import '../styles.css'
 
 
 const Header = () => {
 
 
     return(
-        <div>
-
+        <div className="w-screen h-16 bg-slate-800 flex items-center drop-shadow-2xl fixed top-0">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+            <h1 className=" text-slate-50 lg:text-4xl lg:pl-8 md:pl-8 pl-4 font-mono md:text-4xl text-lg" >LaunchCode Quiz</h1>
+            <a className=" ml-auto lg:pr-8 md:pr-8 pr-4" href="https://github.com/jediknight813">
+                <i className="fa fa-github" style={{"fontSize": "36px", color: "white"}}></i>
+            </a>
         </div>
     )
 }
+
+
+export default Header
--- src/components/quizMainPage.js
+++ src/components/quizMainPage.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import '../styles.css'
+import Header from "./Header";
 
 
 const QuizMainPage = () => { 
-
-
+    const [SelectQuizState, setSelectQuizState] = useState(true)
+    const [AnswerQuizState, setAnswerQuizState] = useState(false)
+    const [GradeQuizState, setGradeQuizState] = useState(false)
+    const launch_code_quiz = require('../quizzes/LaunchCodeQuiz.json');
+    const quizzes = [launch_code_quiz, launch_code_quiz, launch_code_quiz]
 
     return (
-        <div className="w-screen h-16 bg-slate-800 flex items-center">
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-            <h1 className=" text-slate-50 lg:text-4xl lg:pl-8 md:pl-8 pl-4 italic md:text-4xl text-2xl " >LaunchCode Quiz</h1>
-            <a className=" ml-auto lg:pr-8 md:pr-8 pr-4" href="https://github.com/jediknight813">
-                <i className="fa fa-github" style={{"fontSize": "36px", color: "white"}}></i>
-            </a>
+        <div className=" bg-slate-600 w-screen h-screen min-h-max flex items-center justify-center font-mono">
+            <Header />
+            {SelectQuizState &&
+                <div className="w-full md:w-[800px] h-auto min-h-[80%] rounded-sm shadow-lg mt-16 bg-slate-700 text-slate-50 flex flex-col gap-6">
+                    <h1 className=" text-2xl pt-8 pb-2 self-center">Select Quiz</h1>
+                    {quizzes.map(quiz => <button className="bg-slate-800 md:rounded-full md:w-[90%] md:self-center hover:bg-slate-800/80 h-10 shadow-lg cursor-pointer" key={quiz.name}>{quiz.name}</button>)}
+                </div>
+            }
+            {AnswerQuizState &&
+                <div>
+                    answer quiz
+                </div>
+            }
+            {GradeQuizState &&
+                <div>
+                    grade quiz
+                </div>
+            }
+
         </div>
     )   
 }
