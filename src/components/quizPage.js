34132f500b4248f8e76e732c1e3d55847e4ecd0c
-THE-LAST-SAMURAI- <connor813@outlook.com>
2022-05-17 13:08:14 -0500

    Initial commit

---
+++ src/components/quizPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const QuizPage = () => { 
+
+
+
+return (
+    <div>
+        hello world
+    </div>
+)   
+}
+
+
+export default QuizPage
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import QuizPage from './components/quizPage';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <QuizPage />
+  </React.StrictMode>
+);
